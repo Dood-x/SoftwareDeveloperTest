446b37bc528ce9d638237eb47254f5b2eda2f314
StephanieCheng <stephaniecheng00@gmail.com>
2024-04-22 18:19:18 +0200

    added Q5 files, updated readme with instruction

---
+++ Q5/TFS/data/spells/scripts/attack/frigo.lua
@@ -0,0 +1,140 @@
+-- interval at which the spell is animated
+local spellInterval = 200
+
+-- area of effect pattens of the spell (this is approximately according to the video in sequence)
+-- 0 is no spell, 1 is spell, 3 is the players position
+
+local area = {
+
+	{
+		{0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 1, 0},
+		{1, 0, 0, 3, 0, 0, 1},
+		{0, 0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0}
+	},
+
+	{
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 1, 0, 1, 0, 0, 0},
+		{0, 0, 0, 3, 0, 0, 1},
+		{0, 1, 0, 0, 0, 0, 0},
+		{0, 0, 1, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0}
+	},
+
+	{
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 1, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 1, 3, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0}
+	},
+
+	{
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 3, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 1, 0, 0, 0}
+	}
+
+}
+
+-- for some reason the spells are flipped up down and left right
+-- the areas have to be flipped right to left and upside down to correspond to the positions written
+-- this makes it easier to write the tables by hand
+function reverseArea(area)
+	local reverseArea = area
+	local areaCount = #area
+
+	for key, value in ipairs(area) do
+
+		local reverseMatrix = {}
+		local areaHeight = #area[key] 
+
+		for keyi, valuei in ipairs(area[key]) do
+
+			local reverseRow = {}
+			local areaWidth = #area[key][keyi]
+
+			for keyj, valuej in ipairs(area[key][keyi]) do
+				reverseRow[areaWidth+1-keyj] = valuej
+			end
+
+			reverseMatrix[areaHeight+1-keyi] = reverseRow
+		end
+
+		reverseArea[key] = reverseMatrix
+	end
+	return reverseArea
+end 
+area = reverseArea(area)
+
+function ReverseTable(t) 
+	local reversedTable = {} 
+	local itemCount = #t 
+	for k, v in ipairs(t) do 
+		reversedTable[itemCount + 1 - k] = v 
+	end 
+	return reversedTable 
+end 
+
+-- we loop the 4 areas 3 times
+local combat = {
+	Combat(),Combat(),Combat(),Combat(),
+	Combat(),Combat(),Combat(),Combat(),
+	Combat(),Combat(),Combat(),Combat()
+}
+
+-- set each combat to ice damage and sprite
+for key, value in ipairs(combat) do
+	value:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+
+	-- note this sprite is bugged on some clients, other sprites work
+	value:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO) 
+
+	-- areas are casted sequentally and loop, starting from 1
+	value:setArea(createCombatArea(area[((key-1) % #area) + 1]))
+
+	function onGetFormulaValues(player, level, magicLevel)
+		local min = (level / 5) + (magicLevel * 5.5) + 25
+		local max = (level / 5) + (magicLevel * 11) + 50
+		return -min, -max
+	end
+	
+	
+	value:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+end
+
+
+function castSpell(combat, creatureId, variant)
+	local creature = Creature(creatureId)
+    if not creature then
+        return
+    end
+    if not combat then
+        print("no combat!")
+        return
+    end
+
+	combat:execute(creature, variant)
+end
+
+function onCastSpell(creature, variant)
+	local creatureId = creature:getId()
+	
+	for key, value in ipairs(combat) do
+		-- add events to cast the spell separated by the interval
+		addEvent(castSpell, spellInterval * (key-1), value, creatureId, variant)
+	end
+
+	return
+end
--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 
 - Test answers for the Tavernlight Games Software Developer position.
 
-note:
-The answers q5-q7 are new/modified fiels from TFS or OTClient
+The answers q5-q7 are new/modified fiels from TFS and OTClient
+
+to acticvate the spell in Q5 type 'frigo' in the chat
+note: some clients / positions do not render the spell properly
+
+to activate Q7 dialog type talkaction /jumpwindow in chat
 
